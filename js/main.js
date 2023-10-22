970f82a4932b5e0cf274c50b5d183d4d5a2178a4
Nicolas <lnicolas.jofre@outlook.com>
2023-10-22 04:20:19 +0200

    Arreglo de archivo main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,3 @@ addEventListener("DOMContentLoaded", () => {
         document.getElementById("menu").classList.toggle("show");
     });
 } );
-
-setTimeout(function(){
-    window.location.replace("https://cuiprotec.com.ar");
-},2000);
