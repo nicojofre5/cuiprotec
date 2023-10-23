e68499e2763eff873832add8fda465965bf21ed7
Nicolas <lnicolas.jofre@outlook.com>
2023-10-23 03:11:00 +0200

    Funcionamiento de phpmailer

--- php/form.php
+++ php/form.php
@@ -1,22 +1,42 @@
-<?php 
+<?php
+
+
+	if($_SERVER['REQUEST_METHOD'] != 'POST'){
+		header("Location:../index.html");
+	}
+	require('phpMailer/PHPMailer.php');
+	require('phpMailer/Exception.php');
+
+	use PHPMailer\PHPMailer\PHPMailer;
+
 	$nombre = $_POST['nombre'];
 	$email = $_POST['email'];
 	$asunto = 'Consulta CUIPROTEC';
 	$mensaje = $_POST['mensaje'];
 
+	$body = <<<HTML
+		<h1>Contacto desde la web</h1>
+		<p>De: $nombre $apellido / $email</p>
+		<h2>Mensaje</h2>
+		$mensaje
+	HTML;
 	
+	$headers = "MIME-Version: 1.0 \r\n";
+	$headers.="Content-type:text/html;
+	charset=utf-8 \r\n";
+
 	//Como llegará el mail 
 
-	$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-	$mensaje .= "Su e-mail es: " .$mail . " \r\n";
-	$mensaje .= "El asunto es : Consulta CUIPROTEC";
-	$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-	$mensaje .= "Enviado el " . date('d/m/Y', time());
-
+	
 	$para = 'consultas@cuiprotec.com.ar';
-	$asunto = 'Consulta de sitio web';
 
-	mail($para, $asunto, $mensaje, $headers);
+	$mailer = new PHPMailer();
+	$mailer->setFrom($email,"$nombre $apellido");
+	$mailer->addAddress('consultas@cuiprotec.com.ar','Sitio web');
+	$mailer->Subject = "Mensaje web: $asunto";
+	$mailer->msgHTML($body);
+	$mailer->AltBody = strip_tags($body);
+	$rta=$mailer->send();
 
 	header('Location:../exito.html');
 
