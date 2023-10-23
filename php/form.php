7ff0d207e5eab745942ce7adee2d869694a5db6c
Nicolas <lnicolas.jofre@outlook.com>
2023-10-24 01:35:08 +0200

    Correccion de archivos de php y composer

--- php/form.php
+++ php/form.php
@@ -1,4 +1,69 @@
+
+
 <?php
+require '../vendor/autoload.php';
+
+//usamos las clases que vamos a utilizar
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$nombre = $_POST["nombre"];
+$email = $_POST["email"];
+$mensaje = $_POST["mensaje"];
+$asunto = "Consulta desde la web";
+// instanciamos el objeto de la clase phpmailer
+
+$mail = new PHPMailer(true);
+
+// Datos de la cuenta de correo utilizada para enviar vía SMTP
+$smtpHost = "c1462348.ferozo.com";  // Dominio alternativo brindado en el email de alta 
+$smtpUsuario = "info@cuiprotec.com.ar";  // Mi cuenta de correo
+$smtpClave = "Victoria101*";  // Mi contraseña
+// Email donde se enviaran los datos cargados en el formulario de contacto
+$emailDestino = "consultas@cuiprotec.com.ar";
+
+$mail->Host = $smtpHost;
+$mail->Username = $smtpUsuario;
+$mail->Password = $smtpClave;
+$mail->IsSMTP();
+$mail->SMTPAuth = true;
+$mail->Port = 465;
+$mail->SMTPSecure = 'ssl';
+$mail->IsHTML(true);
+$mail->CharSet = "utf-8";
+
+$mail->setFrom($email,$nombre);
+$mail->addAddress('consultas@cuiprotec.com.ar');
+
+$mail->isHTML(true);
+$mail->Subject= $asunto;
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "{$mensajeHtml} <br /><br />Formulario de ejemplo. By DonWeb<br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n Formulario de ejemplo By DonWeb"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$estadoEnvio = $mail->Send(); 
 
 
+
+
+$mail->From = $email; // Email desde donde envío el correo.
+$mail->FromName = $nombre;
+$mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
+
+$mail->Subject = "DonWeb - Ejemplo de formulario de contacto"; // Este es el titulo del email.
+$mensajeHtml = nl2br($mensaje);
+$mail->Body = "{$mensajeHtml} <br /><br />Formulario de ejemplo. By DonWeb<br />"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje} \n\n Formulario de ejemplo By DonWeb"; // Texto sin formato HTML
+// FIN - VALORES A MODIFICAR //
+
+$estadoEnvio = $mail->Send();
+if ($estadoEnvio) {
+    echo "El correo fue enviado correctamente.";
+} else {
+    echo "Ocurrió un error inesperado.";
+}
+
+header("Location:../exito.html");
 ?>
